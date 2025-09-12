33f530d69f78c42e5f5b7806dd6d8b574441c183
Vector-ops <85575949+Vector-ops@users.noreply.github.com>
2025-09-12 23:05:32 +0530

    add math and entity utility functions

--- utils/entity.go
+++ utils/entity.go
@@ -5,3 +5,7 @@ import "github.com/vector-ops/goships/types"
 func ValidateEntityPosition(e types.Entity) bool {
 	return e.StartPosition.X <= 11 && e.StartPosition.Y <= 11 && e.EndPosition.X <= 11 && e.EndPosition.Y <= 11
 }
+
+func ExpectedEndPosition(start int, sprite []rune) int {
+	return start + len(sprite) - 1
+}
---
+++ utils/math.go
@@ -0,0 +1,7 @@
+package utils
+
+import "math"
+
+func AbsInt(a int) int {
+	return int(math.Abs(float64(a)))
+}
